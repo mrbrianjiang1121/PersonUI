bc3291585b1e53a99d476a86779bc6ce481b6932
mrbrianjiang1121 <byjiang@outlook.com>
2019-06-12 15:11:50 -0500

    Completed third project

---
+++ PersonUI/PersonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by Brian Jiang on 6/11/19.
+//  Copyright © 2019 Brian Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
